6f10f1e00a7d20eea825a19c91f5dae7c0ccea8a
Ryan <ryan.n.foote@gmail.com>
2020-05-21 03:44:11 -0500

    Added RESOLUTIONS and started on WHOIS

--- PT2excel.py
+++ PT2excel.py
@@ -17,8 +17,8 @@ workbook_name = target_domain + ".xlsx"
 workbook = xlsxwriter.Workbook(f"{workbook_name}")
 #worksheet_header_format = workbook.add_Format({"bold": True, "font_size": 16})
 #column_header_format = workbook.add_format({"bold": True, "font_size": 12, "bg_color": "#808080", "border": 1})
-row = 2
-column = 0
+#row = 2
+#column = 0
 
 # Gotta figure out how to do this with a loop.
 # def excel_setup():
@@ -34,19 +34,45 @@ def PT_get(path, query):
     response = requests.get(url, auth=auth, json=PT_data)
     return response.json()
 
-# GET Resolutions
+# GET Resolutions (Resolve, [[Location]], [[Network]], [[ASN]], First, Last, Source) Note: don't see location feature in docs.. perhaps external API?
+# Set up worksheet
+RESOLUTIONS_wkst = workbook.add_worksheet("Resolutions")
+RESOLUTIONS_wkst.write("A1", "Resolutions")
+# Call API
+pDNS_results = PT_get("/v2/dns/passive", target_domain)
+for resolve in pDNS_results["results"]:
+    RESOLUTIONS_wkst.write(2, 3, resolve)
+    row += 1
+for firstSeen in pDNS_results["results"]:
+    RESOLUTIONS_wkst.write(3, 3, firstSeen)
+    row += 1
+for lastSeen in pDNS_results["results"]:
+    RESOLUTIONS_wkst.write(4, 3, lastSeen)
+    row += 1
+for source in pDNS_results["results"]:
+    RESOLUTIONS_wkst.write(3, 0, source)
+    row += 1
 
 # GET Whois
+# Set up worksheet
+WHOIS_wkst = workbook.add_worksheet("WhoIs")
+WHOIS_wkst.write("A1", "Whois")
+# Call API
+whois_results = PT_get("https://api.passivetotal.org/v2/whois", target_domain)
+
 
 # GET Certificates
 
 # GET Subdomains
-subdomains_results = PT_get("/v2/enrichment/subdomains", target_domain)
+# Set up worksheet
 SUBDOMAINS_wkst = workbook.add_worksheet("Subdomains")
-SUBDOMAINS_wkst.write("A1", "Subdomains)
+SUBDOMAINS_wkst.write("A1", "Subdomains")
+# Call API
+subdomains_results = PT_get("/v2/enrichment/subdomains", target_domain)
 for subdomains in subdomains_results["subdomains"]:
     # print(f"Found subdomain: {subdomains}" + "." + target_domain)
-    SUBDOMAINS_wkst.write(row, column, subdomains)
+    full_subdomain = subdomains + "." + target_domain
+    SUBDOMAINS_wkst.write(2, 0, full_subdomain)
     row += 1
 
 # Get Trackers
